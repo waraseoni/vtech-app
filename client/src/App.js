03308530b0e0a67e42e2a6f039d17861fce6e51f
waraseoni <vtech.jbp@gmail.com>
2026-02-08 01:43:20 +0530

    Initial MERN project commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = "https://vtech-app.onrender.com";
+const API_URL = "http://localhost:5000";
 
 // Login Component (Inline)
 const Login = ({ setToken, setUser }) => {
